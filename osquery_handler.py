bfc0a3fc9e7bd0f884ccd57c90cd6e43a616854c
ekstrah <dongho@ekstrah.com>
2020-01-03 11:41:56 -0500

    cpu_intensive added

--- osquery_handler.py
+++ osquery_handler.py
@@ -1,10 +1,9 @@
 import osquery
 import re
 import json
-from json2html import *
 
 
-def send_and_receive_all():
+def process_by_username():
     instance = osquery.SpawnInstance()
     instance.open()
     a = str(instance.client.query("SELECT pid, username, name FROM processes p JOIN users u ON u.uid = p.uid ORDER BY start_time"))
@@ -12,6 +11,26 @@ def send_and_receive_all():
     #print(aSplited[0][:-2]) #This is the statusContext
     data = aSplited[1][:-1] #This is the conetent 
     data = data.replace("'", '"')
+    return data
+
+def process_by_cpu_from_bootup():
+    instance = osquery.SpawnInstance()
+    instance.open()
+    a = str(instance.client.query("SELECT pid, uid, name, ROUND((\
+                                  (user_time + system_time) / (cpu_time.tsb - cpu_time.itsb)\
+                                  ) * 100, 2) AS percentage\
+                                  FROM processes, (\
+                                  SELECT (\
+                                  SUM(user) + SUM(nice) + SUM(system) + SUM(idle) * 1.0) AS tsb,\
+                                  SUM(COALESCE(idle, 0)) + SUM(COALESCE(iowait, 0)) AS itsb\
+                                  FROM cpu_time\
+                                  ) AS cpu_time\
+                                  ORDER BY user_time+system_time DESC;"))
+    aSplited = a.rsplit("response=")
+    #print(aSplited[0][:-2]) #This is the statusContext
+    data = aSplited[1][:-1] #This is the conetent 
+    data = data.replace("'", '"')
+    print(data)
     return data
 
 
--- web.py
+++ web.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask import render_template
-from osquery_handler import send_and_receive_all
+from osquery_handler import process_by_username, process_by_cpu_from_bootup
 from json2html import *
 from flask import render_template
 
@@ -13,10 +13,20 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    data = send_and_receive_all()
+    data = process_by_username()
     html_parser = json2html.convert(json=data)
     html_parser = html_parser.replace('<table border="1">', "<table class=container>")
     with open("./templates/data.html", "w") as f:
         f.write(html_parser)
 
     return render_template("index.html", title="OS Interface")
+
+@app.route("/cpu_intensive")
+def cpu_intensive():
+    data = process_by_cpu_from_bootup()
+    html_parser = json2html.convert(json=data)
+    html_parser = html_parser.replace('<table border="1">', "<table class=container>")
+    with open("./templates/data_cpu.html", "w") as f:
+        f.write(html_parser)
+
+    return render_template("cpu.html", title="OS Interface")
